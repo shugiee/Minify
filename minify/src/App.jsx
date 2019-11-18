b5ec196ee0f539771728b4ca11b66755eddb6519
Jonathan <jonathanholson@gmail.com>
2019-11-17 21:22:28 -0800

    Porting To Official Spotify API

--- minify/src/App.jsx
+++ minify/src/App.jsx
@@ -4,19 +4,26 @@ import './App.css';
 
 import * as spotify from './spotify';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <h1>minify</h1>
-        <div>Welcome to minify!!</div>
-        <button id="play">Play</button>
+class App extends React.Component {
+  constructor() {
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1>minify</h1>
+          <div>Welcome to minify!!</div>
+          <h2>User: {user}</h2>
+          <button id="play">Play</button>
+        </div>
+        <script>
+          document.querySelector('#play').addEventListener('click', playSong);
+        </script>
       </div>
-      <script>
-        document.querySelector('#play').addEventListener('click', playSong);
-      </script>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
