1329919d7fbaf0d06a233bfe562c5ce1dab04f06
MyNameIsJonathan <jonathanholson@gmail.com>
2020-01-26 20:34:21 +0000

    Changed Served Material From Spotify/public to Minify/build

---
+++ spotify/public/static/precache-manifest.4a8f5d4b3b7ce07ad1ae51718c72b1ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11bef0e0cb08608a6bf244ed44cb0d4b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "205fa1454412dedc7b62",
+    "url": "./static/css/main.bcdf936a.chunk.css"
+  },
+  {
+    "revision": "046ad93de90603d2954d",
+    "url": "./static/js/2.4a3d6d56.chunk.js"
+  },
+  {
+    "revision": "205fa1454412dedc7b62",
+    "url": "./static/js/main.6c9e2cc0.chunk.js"
+  },
+  {
+    "revision": "91f888c9eb818d2e84d7",
+    "url": "./static/js/runtime-main.25460db1.js"
+  }
+]);
